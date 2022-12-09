968ee59a844ec693a5c5a808bab8ea7b0f3044c7
jmoira <jen.mcquerry98@gmail.com>
2022-12-09 13:46:25 -0500

    add doPost method and link to form

--- icecream-app/WEB-INF/src/HomeServlet.java
+++ icecream-app/WEB-INF/src/HomeServlet.java
@@ -17,6 +17,20 @@ public class HomeServlet extends HttpServlet {
         out.println("<head><title>My Icecream Shop!</title></head>");
         out.println("<body>");
         out.println("<h1>Welcome To My Ice Cream Parlor</h1>");
+        out.println("<a href='/icecream-app/form.html'>Place An Order!</a>");    // link to form
+        out.println("</body></html>");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String cupOrCone = request.getParameter("cupOrCone");
+        String size = request.getParameter("size");
+        String flavor = request.getParameter("flavor");
+
+        PrintWriter out = response.getWriter();    // Get the response PrintWriter
+        out.println("<html>");
+        out.println("<head><title>My Icecream Shop!</title></head>");
+        out.println("<body>");
+        out.println(String.format("<h1>Here is your %s %s ice cream in a %s. Enjoy!</h1>", size, flavor, cupOrCone));
         out.println("</body></html>");
     }
 
